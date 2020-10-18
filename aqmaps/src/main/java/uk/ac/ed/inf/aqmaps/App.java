c658b4998172b05ad91b09fbc5f7d33f44fd3e67
Lorna Armstrong <s1812323@ed.ac.uk>
2020-10-18 14:59:57 +0100

    Get input

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
@@ -8,6 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Test" );
+        // Get the input
+        int day = Integer.parseInt(args[0]);
+        int month = Integer.parseInt(args[1]);
+        int year = Integer.parseInt(args[2]);
+        double startLatitude = Double.parseDouble(args[3]);
+        double startLongitude = Double.parseDouble(args[4]);
+        int seed = Integer.parseInt(args[5]);
+        int portNumber = Integer.parseInt(args[6]);
+       
     }
 }
