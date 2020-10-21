b2c59702cf3cda5e76502fc0835a2428f7047bda
Lorna Armstrong <s1812323@ed.ac.uk>
2020-10-21 10:27:36 +0100

    Add getters and setters for sensor

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
@@ -6,13 +6,38 @@ package uk.ac.ed.inf.aqmaps;
  */
 public class Sensor {
 	
-	private final String location;
-	private final double batteryPercentage;
-	private final double reading;
+	private String location;
+	private double batteryPercentage;
+	private double reading;
 	
 	public Sensor(String location, double batteryPercentage, double reading) {
 		this.location = location;
 		this.batteryPercentage = batteryPercentage;
+		this.reading = reading;
+	}
+	
+	// Getters and Setters
+	public String getLocation() {
+		return this.location;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	public double getBatteryPercentage() {
+		return this.batteryPercentage;
+	}
+	
+	public void setBatteryPercentage(Double batteryPercentage) {
+		this.batteryPercentage = batteryPercentage;
+	}
+	
+	public double getReading() {
+		return this.reading;
+	}
+	
+	public void setReading(Double reading) {
 		this.reading = reading;
 	}
 
