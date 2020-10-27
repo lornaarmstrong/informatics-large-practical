2d626580e5fbb395304da26599090ec139e784de
Lorna Armstrong <s1812323@ed.ac.uk>
2020-10-27 12:24:58 +0000

    Replace variable types with var where possible

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
@@ -50,38 +50,35 @@ public class Sensor {
 		this.reading = reading;
 	}
 	
-	// Find the latitude and longitude for the what3words location
+	/*
+	 *  Find the latitude and longitude for the what3words location, returned as a Coordinate
+	 */
 	public Coordinate getCoords() throws IOException, InterruptedException {
 		
 		// Split the what3words into the 3 separate words
-		String[] words = location.split("\\.");
+		var words = location.split("\\.");
 		
 		// Send a GET request to the server to get the what3words details
         var client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
         		.uri(URI.create("http://localhost:80/words/" + words[0] + "/" + words[1] + "/" + words[2] + "/details.json"))
         		.build();
-        
         var response = client.send(request, BodyHandlers.ofString());
         
         // Check the response.statusCode()
         if (response.statusCode() == 200) {
         	System.out.println("Response recieved correctly, server is working.");
         	// Get the latitude and longitude and make it a Coordinate
-        	
         	var word = new Gson().fromJson(response.body(), Word.class);
-        	        
         	// Get the coordinates of the location
-        	double longitude = word.getCoordinates().getLng();
-        	double latitude = word.getCoordinates().getLat();
+        	var longitude = word.getCoordinates().getLng();
+        	var latitude = word.getCoordinates().getLat();
         	Coordinate coordinate = new Coordinate(latitude, longitude);
         	return coordinate;
-        	
         } else {
         	System.out.println("Error - response not found.");
         }
         
 		return null;
-        
 	}
 }
