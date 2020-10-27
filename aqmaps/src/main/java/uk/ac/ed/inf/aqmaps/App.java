2b0cee5fd2c8f23a2a7b1625dbf64cae4f142830
Lorna Armstrong <s1812323@ed.ac.uk>
2020-10-27 14:16:00 +0000

    Apply style guide

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class App 
 {
 	
-	//Initialise Variables
+	// Initialise Variables
 	public static List<Sensor> sensorList = new ArrayList<Sensor>();
 	
     public static void main( String[] args ) throws IOException, InterruptedException
@@ -32,22 +32,19 @@ public class App
         int seed = Integer.parseInt(args[5]);
         int portNumber = Integer.parseInt(args[6]);
     	
-        
         sensorList = getSensorsFromServer(day, month, year);
-        
-        for (Sensor s: sensorList) {
-        	System.out.println(s.getLocation() + " ayyy");
-        }
        
     }
 
     /*
      * Get the list of all sensors to be visited on the given date (from input) 
      */
-	public static List<Sensor> getSensorsFromServer(String day, String month, String year) throws IOException, InterruptedException {
+	public static List<Sensor> getSensorsFromServer(String day, String month, String year) 
+				throws IOException, InterruptedException {
 		var client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-        		.uri(URI.create("http://localhost:80/maps/" + year + "/" + month + "/" + day + "/air-quality-data.json"))
+        		.uri(URI.create("http://localhost:80/maps/" + year + "/" + month + "/" + day
+        				+ "/air-quality-data.json"))
         		.build();
         var response = client.send(request, BodyHandlers.ofString());
         
