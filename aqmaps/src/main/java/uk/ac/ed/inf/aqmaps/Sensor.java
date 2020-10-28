c7916621e23f745e20565664c6b7b528a4df07b2
Lorna Armstrong <s1812323@ed.ac.uk>
2020-10-28 08:51:23 +0000

    FIx indentation

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
@@ -54,33 +54,33 @@ public class Sensor {
 	 *  Find the latitude and longitude for the what3words location, returned as a Coordinate
 	 */
 	public Coordinate getCoordinates() throws IOException, InterruptedException {
-	    // Split the what3words into the 3 separate words
-	    var words = location.split("\\.");
+	        // Split the what3words into the 3 separate words
+	        var words = location.split("\\.");
 	    
-	    // Send a GET request to the server to get the what3words details
-	    var client = HttpClient.newHttpClient();
-	    HttpRequest request = HttpRequest.newBuilder()
-	            .uri(URI.create("http://localhost:" + App.portNumber + "/words/" + words[0] + "/" 
+	        // Send a GET request to the server to get the what3words details
+	        var client = HttpClient.newHttpClient();
+	        HttpRequest request = HttpRequest.newBuilder()
+	                .uri(URI.create("http://localhost:" + App.portNumber + "/words/" + words[0] + "/" 
 	                     + words[1] + "/" + words[2] + "/details.json"))
-        		.build();
-	    var response = client.send(request, BodyHandlers.ofString());
-	    // Check the response.statusCode()
-	    int statusCode = response.statusCode();
-	    if (statusCode == 200) {
-	        System.out.println("Response recieved correctly.");
-	        // Get the latitude and longitude and make it a Coordinate
-	        var word = new Gson().fromJson(response.body(), Word.class);
-	        // Get the coordinates of the location
-	        var longitude = word.getCoordinates().getLng();
-	        var latitude = word.getCoordinates().getLat();
-	        Coordinate coordinate = new Coordinate(latitude, longitude);
-	        return coordinate;
-	    } else if (statusCode == 404){
-	        // There is an error with the request
-	        System.out.println("The server cannot find the requested resource [error 404]");
-	    } else {
-	        // For other server status codes
-	        System.out.println("The status code is " + statusCode);
+        		    .build();
+	        var response = client.send(request, BodyHandlers.ofString());
+	        // Check the response.statusCode()
+	        int statusCode = response.statusCode();
+	        if (statusCode == 200) {
+	            System.out.println("Response recieved correctly.");
+	            // Get the latitude and longitude and make it a Coordinate
+	            var word = new Gson().fromJson(response.body(), Word.class);
+	            // Get the coordinates of the location
+	            var longitude = word.getCoordinates().getLng();
+	            var latitude = word.getCoordinates().getLat();
+	            Coordinate coordinate = new Coordinate(latitude, longitude);
+	            return coordinate;
+	        } else if (statusCode == 404){
+	            // There is an error with the request
+	            System.out.println("The server cannot find the requested resource [error 404]");
+	        } else {
+	            // For other server status codes
+	            System.out.println("The status code is " + statusCode);
 	    }
 	    return null;
 	}
