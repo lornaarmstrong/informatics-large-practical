7f3e5d47c9b0a06a51bd69a5b3f6a4ff30bbc5f0
Lorna Armstrong <s1812323@ed.ac.uk>
2020-11-23 19:51:31 +0000

    Removed unused attributes

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Word.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Word.java
@@ -1,17 +1,8 @@
 package uk.ac.ed.inf.aqmaps;
 
-import com.mapbox.geojson.Point;
-
 public class Word {
-
-	// Matching the words details.json structure from the server
-	private String country;
-	private Square square;
-	private String nearestPlace;
+    
 	private LngLat coordinates;
-	private String words;
-	private String language;
-	private String map;
 	
 	// Getters and Setters
 	
