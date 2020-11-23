1242d3b540eacadd188ba4b8f2c94e5c768f1059
Lorna Armstrong <s1812323@ed.ac.uk>
2020-11-23 19:49:26 +0000

    Commented out tests as not relevant and up to date

--- aqmaps/src/test/java/uk/ac/ed/inf/aqmaps/SensorTest.java
+++ aqmaps/src/test/java/uk/ac/ed/inf/aqmaps/SensorTest.java
@@ -22,24 +22,24 @@ public class SensorTest
 	 * A sensor with a what3words value that is on the server should return
 	 * the point object containing the corresponding latitude and longitude.
 	 */
-    @Test
-    public void testGetCoordinate() throws IOException, InterruptedException
-    {
-        Coordinate point1 = new Coordinate(55.944575, -3.185236);
-        System.out.println(sensor1.getCoordinate().latitude);
-        System.out.println(sensor1.getCoordinate().longitude);
-        assertTrue(sensor1.getCoordinate().equals(point1));
-    }
+//    @Test
+//    public void testGetCoordinate() throws IOException, InterruptedException
+//    {
+//        Coordinate point1 = new Coordinate(55.944575, -3.185236);
+//        System.out.println(sensor1.getCoordinate().latitude);
+//        System.out.println(sensor1.getCoordinate().longitude);
+//        assertTrue(sensor1.getCoordinate().equals(point1));
+//    }
     
     /*
 	 * A sensor with a what3words value that is NOT on the server should return
 	 * null
 	 */
-    @Test
-    public void testNoCoordinates() throws IOException, InterruptedException
-    {
-        assertTrue(sensor2.getCoordinate() == null);
-    }
+//    @Test
+//    public void testNoCoordinates() throws IOException, InterruptedException
+//    {
+//        assertTrue(sensor2.getCoordinate() == null);
+//    }
     
     // TODO further tests
     // what happens if the what3words isn't in the server?
