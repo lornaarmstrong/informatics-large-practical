85b80841211b745342d36f637546d972120fd140
Lorna Armstrong <s1812323@ed.ac.uk>
2020-11-26 10:26:41 +0000

    Order import statements

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
@@ -1,12 +1,10 @@
 package uk.ac.ed.inf.aqmaps;
 
+import com.google.gson.Gson;
+import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
-
-import com.google.gson.Gson;
-
-import java.io.IOException;
 import java.net.URI;
 
 /**
