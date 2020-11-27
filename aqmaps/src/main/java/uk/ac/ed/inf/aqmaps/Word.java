a4acdbdf1577fa4ad7b64b3bee9699cc5f0fba5c
Lorna Armstrong <s1812323@ed.ac.uk>
2020-11-27 12:14:19 +0000

    Add class comment to Word

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Word.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Word.java
@@ -1,11 +1,14 @@
 package uk.ac.ed.inf.aqmaps;
 
+/*
+ * A class to contain the Word objects when the JSON
+ * is parsed from the server.
+ */
 public class Word {
     
 	private LngLat coordinates;
 	
-	// Getters and Setters
-	
+	/* Getters and Setters */
 	public LngLat getCoordinates() {
 		return this.coordinates;
 	}
@@ -13,6 +16,4 @@ public class Word {
 	public void setCoordinates(LngLat coordinates) {
 		this.coordinates = coordinates;
 	}
-	
-	
 }
