200b86653834415c7d6bdf37af097670251b73ee
Lorna Armstrong <s1812323@ed.ac.uk>
2020-12-02 21:41:55 +0000

    Refactor Line

---
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/GeographicalArea.java
@@ -0,0 +1,143 @@
+package uk.ac.ed.inf.aqmaps;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * GeographicalArea class represents a map of Sensors and No Fly Zones on a specific day
+ */
+public class GeographicalArea {
+    
+    private final String day;
+    private final String month;
+    private final String year;
+    public final List<Sensor> sensors = new ArrayList<Sensor>();
+    public final List<Feature> noFlyZones = new ArrayList<Feature>();
+    public final double[][] distanceMatrix = new double [34][34];
+    private static final HttpClient httpClient = HttpClient.newHttpClient();
+    
+    // Confinement Zone Coordinates
+    public static final Coordinate topLeftConfinement = new Coordinate(55.946233, -3.192473);
+    public static final Coordinate topRightConfinement = new Coordinate(55.946233, -3.184319);
+    public static final Coordinate bottomLeftConfinement = new Coordinate(55.942617, -3.192473);
+    public static final Coordinate bottomRightConfinement = new Coordinate(55.942617, -3.184319);
+    
+    public GeographicalArea(String day, String month, String year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    
+    /*
+     * Call all methods within GeographicalArea needed to set up the map.
+     */
+    public void setUp(int portNumber, Coordinate startPosition) 
+            throws IOException, InterruptedException {
+        // Get sensor and no fly zone information from server
+        getSensorListFromServer(portNumber);
+        getNoFlyZonesFromServer(portNumber);
+        // Get the latitude and longitude values of each sensor using the server and store
+        for (var sensor: sensors) {
+            sensor.translateLocation();
+        }
+        
+        removeOutsideSensors();
+        calculateDistanceMatrix(startPosition);
+    }
+    
+    /*
+     * Get the list of all sensors to be visited, from the server, and add each sensor to sensors
+     */
+    public void getSensorListFromServer(int portNumber) throws IOException, InterruptedException {
+        System.out.println("Retrieving list of sensors from server.");
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(String.format("http://localhost:%d/maps/%s/%s/%s/" 
+                        + "air-quality-data.json", portNumber,this.year,this.month,this.day)))
+                .build();
+        var response = httpClient.send(request, BodyHandlers.ofString());
+        var listType = new TypeToken<ArrayList<Sensor>>(){}.getType();
+        List<Sensor> sensorsForThisDay = new Gson().fromJson(response.body(), listType);
+        for (var sensor: sensorsForThisDay) {
+            sensors.add(sensor);
+        }
+        System.out.println("Number of sensors retrieved from server: " + sensors.size());
+    }
+    
+    /*
+     * Get the list of all polygons representing the No-Fly Zone buildings, from the server.
+     */
+    
+    public void getNoFlyZonesFromServer(int portNumber) throws IOException, InterruptedException {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:" + portNumber
+                        + "/buildings/no-fly-zones.geojson"))
+                .build();
+        var response = httpClient.send(request, BodyHandlers.ofString());
+        var featureCollection = FeatureCollection.fromJson(response.body());
+        var featureList = featureCollection.features();
+        // Adds each feature to the list of No Fly Zones
+        for (var feature : featureList) {
+            noFlyZones.add(feature);
+        }
+        System.out.println("No Fly Zones retrieved from server: " + noFlyZones.size());
+    }
+    
+    /*
+     * Remove any sensors that are outside the confinement zone from sensors list.
+     */
+    public void removeOutsideSensors() {
+        var indexesToRemove = new ArrayList<>();
+        for (var i = 0; i < this.sensors.size(); i++ ) {
+            var sensor = this.sensors.get(i);
+            if (!sensor.getCoordinate().isInConfinementZone(topLeftConfinement, 
+                    bottomLeftConfinement, bottomRightConfinement)) {
+                indexesToRemove.add(i);
+            }
+        } 
+        for (var i = 0; i < indexesToRemove.size(); i ++) {
+            var index = indexesToRemove.get(i);
+            this.sensors.remove(index);
+        }
+    }
+    
+    /*
+     * Fill a 34 x 34 grid with the distances from all sensors to each other sensor, and 
+     * all sensors to the start.
+     */
+    public void calculateDistanceMatrix(Coordinate droneStartPosition) {
+        for (int i = 0; i < this.distanceMatrix.length; i++) {
+            for (int j = 0; j < this.distanceMatrix.length; j++) {
+                if (i == j) {
+                    /* Set distance from i to itself to be a large number to avoid using in 
+                      minimum distance algorithm */
+                    this.distanceMatrix[i][j] = 1000;
+                } else {
+                    Coordinate destination;
+                    Coordinate startFrom;
+                    if (i == 0) {
+                        destination = sensors.get(j - 1).getCoordinate();
+                        startFrom = droneStartPosition;
+                    } else {
+                        if (j == 0) {
+                            destination = droneStartPosition;
+                            startFrom = sensors.get(i - 1).getCoordinate();
+                        } else {
+                            startFrom = sensors.get(i - 1).getCoordinate();
+                            destination = sensors.get(j - 1).getCoordinate();
+                        }
+                    }
+                    this.distanceMatrix[i][j] = startFrom.getEuclideanDistance(destination);
+                }
+            }
+        }
+    }
+}
--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Line.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Line.java
@@ -25,7 +25,7 @@ public class Line {
     }
 
     /*
-     * Checks if the line intersects with a passed-in line
+     * Check if the line intersects with a passed-in line
      */
     public boolean isIntersecting(Line boundary) {
         
@@ -40,6 +40,7 @@ public class Line {
         var Y4 = boundary.getCoordinateB().getLatitude();
         
         // Check if there is any overlap in the X values of the two lines
+        // If not, return false since there's no possible point of intersection.
         if ( Math.max(X1, X2) < Math.min(X3, X4)) {
             return false;
         }
@@ -47,46 +48,54 @@ public class Line {
         else {
             // Check if the gradient is infinite
             if (X1 == X2) {
-                // see if X1 lies on the equation of the Y3, Y4 line
+                
+               // Get the Y value for the X1 value, on the line through Y3,Y4
                 var m2 = (Y3 - Y4) / (X3 - X4);
                 var c2 = Y3 - (m2 * X3);
                 var yValueOfX1 = (m2 * X1) + c2;
                 
                 if (yValueOfX1 >= Math.min(Y1, Y2) && yValueOfX1 <= Math.max(Y1, Y2)) {
+                    // The lines intersect within the possible Y values of the line segments.
                     return true;
                 } else {
+                    // The infinite lines intersect but the intersection point isn't within the 
+                    // possible Y value interval of the line segments.
                     return false;
                 } 
             }
             if (X3 == X4) {
-                // see if X1 lies on the equation of the Y3, Y4 line
+                // See if X3 lies on the equation of the Y1, Y2 line
                 var m = (Y1 - Y2) / (X1 - X2);
                 var c = Y1 - (m * X1);
                 var yValueOfX3 = (m * X1) + c;
                 
                 if (yValueOfX3 > Math.min(Y3, Y4) && yValueOfX3 < Math.max(Y3, Y4)) {
+                    // The lines intersect within the possible Y values of the line segments.
                     return true;
                 } else {
+                    // The infinite lines intersect but the intersection point isn't within the 
+                    // possible Y value interval of the line segments
                     return false;
                 }
             } 
             
+            // Get the equations of both infinite lines
             var m1 = (Y1 - Y2) / ( X1 - X2);
             var m2 = (Y3 - Y4) / (X3 - X4);
             var c1 = Y1 - (m1 * X1);
             var c2 = Y3 - (m2 * X3);
             
             // Check if the two lines have the same gradient
-            if (m1 == m2) {
+            if (m1 == m2 && X1 != X3) {
+                // The lines are parallel, so do not intersect.
                 return false;
             }
             
-            // Check if (X,Y) lies on both lines and is the intersection point.
+            // Check if (X,Y) lies on both lines and intersects at a point within 
+            // the line segments.
             var intersectX = (c2 - c1) / (m1 - m2);
             var possibleY = (m1 * intersectX) + c1;
             var possibleY2 = (m2 * intersectX) + c2;
-            
-            // Check if (X,Y) lies in the interval of the line segments
             if (Math.abs(possibleY - possibleY2) < THRESHOLD) {
               if ( intersectX < Math.max(Math.min(X1, X2), Math.min(X3, X4)) 
                       || intersectX > Math.min(Math.max(X1, X2), Math.max(X3, X4))) {
